081055f05cd80e114210bed27d791fcdc685bb54
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-05-31 15:53:57 -0400

    live code solution

--- server.rb
+++ server.rb
@@ -1,15 +1,16 @@
-require 'Sinatra'
+require 'sinatra'
 require 'pry'
 
 get '/' do
-  ingredients = File.readlines('ingredients.txt')
+  @ingredients = File.readlines('ingredients.txt')
+
   erb :index
 end
 
 post '/ingredients' do
   ingredient = params['ingredient']
 
-  File.open('ingredients.txt', 'w') do |file|
+  File.open('ingredients.txt', 'a') do |file|
     file.puts(ingredient)
   end
 
